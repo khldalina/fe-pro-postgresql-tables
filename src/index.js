1689224785187105cc8275ae39805f0dfd85cd73
Alina <khldalina@gmail.com>
2022-12-02 14:20:23 +0200

    added new code

--- src/index.js
+++ src/index.js
@@ -23,8 +23,60 @@ export const createStructure = async () => {
   const client = initConnection();
   client.connect();
 
-  // Your code is here...
-  // Your code is here...
+  await client.query(`
+    CREATE TABLE IF NOT EXISTS users(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR (30) UNIQUE NOT NULL,
+      date TIMESTAMP DEFAULT CURRENT_DATE
+    );
+  `);
+
+  await client.query(`
+    CREATE TABLE IF NOT EXISTS categories(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR (30) UNIQUE NOT NULL
+    );
+  `);
+
+  await client.query(`
+    CREATE TABLE IF NOT EXISTS authors(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR (30) UNIQUE NOT NULL
+    );
+  `);
+
+  await client.query(`
+    CREATE TABLE IF NOT EXISTS books(
+      id SERIAL PRIMARY KEY,
+      title VARCHAR (30) NOT NULL,
+      userid INTEGER NOT NULL,
+      authorid INTEGER NOT NULL,
+      categoryid INTEGER NOT NULL,
+      FOREIGN KEY (userid) REFERENCES users (id) ON DELETE CASCADE,
+      FOREIGN KEY (authorid) REFERENCES authors (id) ON DELETE CASCADE,
+      FOREIGN KEY (categoryid) REFERENCES categories (id) ON DELETE CASCADE
+    );
+  `);
+
+  await client.query(`
+    CREATE TABLE IF NOT EXISTS descriptions(
+      id SERIAL PRIMARY KEY,
+      description VARCHAR (10000) UNIQUE NOT NULL,
+      bookid INTEGER NOT NULL,
+      FOREIGN KEY (bookid) REFERENCES books (id) ON DELETE CASCADE
+    );
+  `);
+
+  await client.query(`
+    CREATE TABLE IF NOT EXISTS reviews(
+      id SERIAL PRIMARY KEY,
+      message VARCHAR (10000) NOT NULL,
+      userid INTEGER NOT NULL,
+      bookid INTEGER NOT NULL,
+      FOREIGN KEY (userid) REFERENCES users (id) ON DELETE CASCADE,
+      FOREIGN KEY (bookid) REFERENCES books (id) ON DELETE CASCADE
+    );
+  `);
 
   client.end();
 };
@@ -33,7 +85,6 @@ export const createItems = async () => {
   const client = initConnection();
   client.connect();
 
-  // Your code is here...
 
   client.end();
 };
@@ -42,12 +93,12 @@ export const dropTables = async () => {
   const client = initConnection();
   client.connect();
 
-  await client.query('DROP TABLE reviews;');
-  await client.query('DROP TABLE descriptions;');
-  await client.query('DROP TABLE books;');
-  await client.query('DROP TABLE authors;');
-  await client.query('DROP TABLE categories;');
-  await client.query('DROP TABLE users;');
+  await client.query("DROP TABLE reviews;");
+  await client.query("DROP TABLE descriptions;");
+  await client.query("DROP TABLE books;");
+  await client.query("DROP TABLE authors;");
+  await client.query("DROP TABLE categories;");
+  await client.query("DROP TABLE users;");
 
   client.end();
 };
